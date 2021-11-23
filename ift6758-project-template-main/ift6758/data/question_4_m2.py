updated Question_2_M2 and fixed an error in coord computation in question_4_m2.py

ift6758-project-template-main/ift6758/data/question_4_m2.py
@@ -20,7 +20,7 @@ def get_new_features(data_frame):
     df['previous_event_game_seconds'] = df2[0]*60+df2[1]
     df['previous_event_game_seconds'] = ((df['previous_event_period']-1)*(60*20) + df['previous_event_game_seconds'])
     df['time_since_last_event'] = df['game_seconds']-df['previous_event_game_seconds']
-    df['distance_from_last_event'] = np.sqrt((df['x_coordinates']**2 - df['previous_event_x_coordinates'])**2 + (df['y_coordinates'] - df['previous_event_y_coordinates'])**2)
+    df['distance_from_last_event'] = np.sqrt((df['x_coordinates'] - df['previous_event_x_coordinates'])**2 + (df['y_coordinates'] - df['previous_event_y_coordinates'])**2)
     df['distance_from_last_event'] = df['distance_from_last_event'].round(decimals=2)
     df['distance_from_net'] = df['distance_from_net'].round(decimals=2)
     rebound = np.empty(len(df.index),dtype=bool)