2613530a9e53a567d9deb3bcc21cee32adff1960
Mahdi <mahdi.kleit@gmail.com>
2021-12-22 14:00:51 -0500

    updated final game_client

--- docker-project-template-main/ift6758/ift6758/client/game_client.py
+++ docker-project-template-main/ift6758/ift6758/client/game_client.py
@@ -5,12 +5,15 @@ import logging
 import os
 import numpy as np
 import math 
+from sklearn.preprocessing import LabelEncoder
 
 class GameClient:
     def __init__(self,game_id):
         self.game_id = game_id
         self.tracker = 0
         self.game = None
+        self.home_team = None
+        self.away_team = None
         
     def get_game(self):
         file_path = './' + str(self.game_id) + '.json'
@@ -25,15 +28,14 @@ class GameClient:
 
 
     def update_events(self):
-        self.get_game()
-        file_path = './' + str(self.game_id) + '.json'
+        file_path = self.get_game()
         attributes = []
         with open(file_path,'r') as f:
             game = json.load(f)
         events = game['liveData']['plays']['allPlays']
         previous_event = None
-        home_team = game['gameData']['teams']['home']['name']
-        away_team = game['gameData']['teams']['away']['name']
+        self.home_team = game['gameData']['teams']['home']['name']
+        self.away_team = game['gameData']['teams']['away']['name']
         
         home_players = 5
         away_players = 5
@@ -72,11 +74,18 @@ class GameClient:
                 x_coordinates = event['coordinates'].get('x')
                 y_coordinates = event['coordinates'].get('y')
                 attacking_team = event['team']['name']
-                period = event['about']['period']
+                attacking_team_side = 'left'
+                period = int(event['about']['period'])
                 period_time = event['about']['periodTime']
+                var = period_time.split(':')
+                period_time = int(var[0])*60+int(var[1])
                 time_left = 20*60 - period_time*60
                 game_seconds = (period-1)*20*60 + period_time*60
                 shot_type = event['result'].get('secondaryType')
+                if event['team'].get('name') == self.home_team:
+                    shot_by_team = "Home"
+                else:
+                    shot_by_team = "Away"
                 if previous_event is None:
                     time_since_last_event = 0
                     previous_event_type = None
@@ -84,24 +93,31 @@ class GameClient:
                     previous_event_y_coordinates = 0
                     previous_event_period_time = 0
                     previous_event_period = 0
+                    distance_from_last_event = 0
+                    speed = 0
                 else:
-                    previous_event_x_coordinates = previous_event['coordinates'].get('x')
-                    previous_event_y_coordinates = previous_event['coordinates'].get('y')
+                    previous_event_x_coordinates = previous_event['coordinates'].get('x',0)
+                    previous_event_y_coordinates = previous_event['coordinates'].get('y',0)
                     previous_event_period_time = previous_event['about']['periodTime']
-                    previous_event_period = previous_event['about']['period']
+                    var_pe = previous_event_period_time.split(':')
+                    previous_event_period_time = int(var_pe[0])*60+int(var_pe[1])
+                    previous_event_period = int(previous_event['about']['period'])
                     time_since_last_event = game_seconds - ((previous_event_period-1)*20*60 + previous_event_period_time*60)
                     previous_event_type = previous_event['result']['event']
-                distance_from_last_event = np.sqrt((x_coordinates**2 - previous_event_x_coordinates**2)+(y_coordinates**2 - previous_event_y_coordinates**2))
-                speed = distance_from_last_event / time_since_last_event
+                    distance_from_last_event = np.sqrt((x_coordinates**2 - previous_event_x_coordinates**2)+(y_coordinates**2 - previous_event_y_coordinates**2))
+                    if time_since_last_event != 0:
+                        speed = distance_from_last_event / time_since_last_event
+                    else:
+                        speed = 0
                 if previous_event['result']['event'] == 'Shot' or previous_event['result']['event'] == 'Goal':
-                        rebound = 1
-                    else: 
-                        rebound = 0
+                    rebound = 1
+                else: 
+                    rebound = 0
 
-                attributes.append([x_coordinates, y_coordinates,period,period_time,previous_event_type,previous_event_x_coordinates, previous_event_y_coordinates,previous_event_period_time,previous_event_period,rebound,home_team,away_team,shot_type,attacking_team,game_seconds,speed,distance_from_last_event,home_players,away_players,time_since_powerplay_started,time_left])
+                attributes.append([x_coordinates, y_coordinates,period,period_time,previous_event_type,previous_event_x_coordinates, previous_event_y_coordinates,previous_event_period_time,previous_event_period,rebound,self.home_team,self.away_team,shot_type,attacking_team,game_seconds,speed,distance_from_last_event,home_players,away_players,time_since_powerplay_started,time_left,attacking_team_side,time_since_last_event,shot_by_team])
                 
             if event['result']['event'] == 'Goal':
-                if attacking_team == home_team:
+                if event['team']['name'] == self.home_team:
                     if (len(penalties['away_minor2_penalty_stack'])>0 and len(penalties['away_minor4_penalty_stack'])>0) :
                         # when there is both a 2min minor and a 4min minor, we will assume the time is taken away from the penalty with the least time left
                         if(penalties['away_minor2_penalty_stack'][0] < penalties['away_minor4_penalty_stack'][0]):
@@ -137,8 +153,9 @@ class GameClient:
                             penalties['home_minor4_penalty_stack'][0] = 0
             
             elif event['result']['event'] == 'Penalty':
-                if event['result']['penaltySeverity'] == 'Minor':
-                    if event['result']['penaltyMinutes'] == 2: 
+                if event['team']['name'] == self.home_team:
+                    if event['result']['penaltySeverity'] == 'Minor':
+                        if event['result']['penaltyMinutes'] == 2: 
                             penalties['home_minor2_penalty_stack'].append(120)
                         else:
                             penalties['home_minor4_penalty_stack'].append(240)
@@ -153,10 +170,13 @@ class GameClient:
                             penalties['away_minor4_penalty_stack'].append(240)
                     else:
                         penalties['away_major_penalty_stack'].append(300)
-                        
+    
             previous_event = event
-        game = pd.DataFrame(attributes, columns=['x_coordinates, y_coordinates,period,period_time,previous_event_type,previous_event_x_coordinates, previous_event_y_coordinates,previous_event_period_time,previous_event_period,rebound,home_team,away_team,shot_type,attacking_team,game_seconds,speed,distance_from_last_event,home_players,away_players,time_since_powerplay_started'])
-        return game
+        df = pd.DataFrame(attributes, columns=['x_coordinates', 'y_coordinates','period','period_time','previous_event_type','previous_event_x_coordinates', 'previous_event_y_coordinates','previous_event_period_time','previous_event_period','rebound','home_team','away_team','shot_type','attacking_team','game_seconds','speed','distance_from_last_event','home_players','away_players','time_since_powerplay_started','time_left','attacking_team_side','time_since_last_event','shot_by_team'])
+        print(df.columns)
+        return df
+    
+    ## to add: penalty kill, powerplay, overtime, rebound_same_team, home_team_attacking
     
     def update_tracker(self):
         self.tracker = self.game.shape[0]
@@ -164,7 +184,7 @@ class GameClient:
     def features_for_models(self,model):
         if model == 'lrd':
             temp = self.game['distance_from_net']
-            return temp.loc[self.tracker:,:]
+            return temp.loc(axis=0)[self.tracker:]
         
         if model == 'lrda':
             temp = self.game[['distance_from_net','angle_from_net']]
@@ -172,9 +192,9 @@ class GameClient:
         
         if model == 'lda':
             temp = self.game['angle_from_net']
-            return temp.loc[self.tracker:,:]
+            return temp.loc(axis=0)[self.tracker:]
         
-        if model == 'xgboost-tuning':
+        if model == 'xgboost-tuning' or model == 'xgboost':
             feat_xgb = ['game_seconds', 'period', 'x_coordinates', 'y_coordinates','distance_from_net', 'angle_from_net', 
                         'previous_event_type','previous_event_x_coordinates', 'previous_event_y_coordinates',
                         'time_since_last_event', 'distance_from_last_event', 'rebound','change_in_angle', 'speed', 'time_since_powerplay_started', 
@@ -184,27 +204,28 @@ class GameClient:
             le_name_mapping = {'Blocked Shot': 0, 'Faceoff': 1, 'Game Official': 2, 'Giveaway': 3, 'Goal': 4, 'Hit': 5, 'Missed Shot': 6, 'Official Challenge': 7, 'Penalty': 8, 'Period End': 9, 'Period Official': 10, 'Period Ready': 11, 'Period Start': 12, 'Shootout Complete': 13, 'Shot': 14, 'Stoppage': 15, 'Takeaway': 16}
     
             temp = self.game
-            temp = self.geat_angle_change(temp)
+            temp = self.get_angle_change(temp)
             temp = self.get_penalties()
+            temp = temp.join(pd.get_dummies(temp['shot_type'],prefix='shot_type'))
+            print(temp.head(5))
             temp = self.preprocess(temp[feat_xgb])
             temp['previous_event_type'] = temp['previous_event_type'].replace(le_name_mapping)
             return temp.loc[self.tracker:,:] 
-            
+        
+    def features_for_dashboard(self):
+        temp = self.game
+        return temp
     
-    def preprocess(self,df):
-        le = LabelEncoder()
-        df["previous_event_type"] = le.fit_transform(df["previous_event_type"])
-        return df
-
     def ping_game(self,model):
         self.game = self.update_events()
         self.game = self.get_distance()
         self.game = self.get_angle()
         df = self.features_for_models(model)
+        dashboard_df = self.features_for_dashboard()
         self.update_tracker()
         tracker = self.tracker
-        return df, tracker
-    
+        return df, tracker, dashboard_df
+   
     def get_distance(self):
         df = self.game
         list_distance = []
@@ -226,45 +247,45 @@ class GameClient:
         df['angle_from_net'] = list_angle
         return df
     
-    def get_angle_change(self,df):
-    df = df
-    list_angle = []
-    for index, row in df.iterrows():
-        if row['previous_event_type']=='Shot':
-            if df.attacking_team_side[index] == "right":
-                if df.previous_event_y_coordinates[index] == 0:
-                    list_angle.append(np.absolute(df.angle_from_net[index]))
-                else:
-                    distance_from_net = np.minimum(np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]+89)**2),np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]-89)**2))
-                    angle = np.arcsin(df.previous_event_y_coordinates[index]/distance_from_net)*-180/math.pi
-                    sign = np.sign([angle,df.angle_from_net[index]])
-                    change_in_angle = 0
-                    if sign[0]!=sign[1]:
-                        change_in_angle = np.absolute(angle) + np.absolute(df.angle_from_net[index])
+    def get_angle_change(self,dataframe):
+        df = dataframe
+        list_angle = []
+        for index, row in df.iterrows():
+            if row['previous_event_type']=='Shot':
+                if df.attacking_team_side[index] == "right":
+                    if df.previous_event_y_coordinates[index] == 0:
+                        list_angle.append(np.absolute(df.angle_from_net[index]))
                     else:
-                        change_in_angle = np.absolute(angle-df.angle_from_net[index])
-                    list_angle.append(change_in_angle)
-                    
-            if df.attacking_team_side[index] == "left":
-                if df.previous_event_y_coordinates[index] == 0:
-                    list_angle.append(np.absolute(df.angle_from_net[index]))
-                else:
-                    distance_from_net = np.minimum(np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]+89)**2),np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]-89)**2))
-                    angle = np.arcsin(df.previous_event_y_coordinates[index]/distance_from_net)*180/math.pi
-                    sign = np.sign([angle,df.angle_from_net[index]])
-                    change_in_angle = 0
-                    if sign[0]!=sign[1]:
-                        change_in_angle = np.absolute(angle) + np.absolute(df.angle_from_net[index])
+                        distance_from_net = np.minimum(np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]+89)**2),np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]-89)**2))
+                        angle = np.arcsin(df.previous_event_y_coordinates[index]/distance_from_net)*-180/math.pi
+                        sign = np.sign([angle,df.angle_from_net[index]])
+                        change_in_angle = 0
+                        if sign[0]!=sign[1]:
+                            change_in_angle = np.absolute(angle) + np.absolute(df.angle_from_net[index])
+                        else:
+                            change_in_angle = np.absolute(angle-df.angle_from_net[index])
+                        list_angle.append(change_in_angle)
+
+                if df.attacking_team_side[index] == "left":
+                    if df.previous_event_y_coordinates[index] == 0:
+                        list_angle.append(np.absolute(df.angle_from_net[index]))
                     else:
-                        change_in_angle = np.absolute(angle-df.angle_from_net[index])
-                    list_angle.append(change_in_angle)  
-        else:
-            list_angle.append(0)
-            
-    df['change_in_angle'] = list_angle
-    df['change_in_angle'] = df['change_in_angle'].round(decimals=2)
-    
-    return df
+                        distance_from_net = np.minimum(np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]+89)**2),np.sqrt(df.previous_event_y_coordinates[index]**2+(df.previous_event_x_coordinates[index]-89)**2))
+                        angle = np.arcsin(df.previous_event_y_coordinates[index]/distance_from_net)*180/math.pi
+                        sign = np.sign([angle,df.angle_from_net[index]])
+                        change_in_angle = 0
+                        if sign[0]!=sign[1]:
+                            change_in_angle = np.absolute(angle) + np.absolute(df.angle_from_net[index])
+                        else:
+                            change_in_angle = np.absolute(angle-df.angle_from_net[index])
+                        list_angle.append(change_in_angle)  
+            else:
+                list_angle.append(0)
+
+        df['change_in_angle'] = list_angle
+        df['change_in_angle'] = df['change_in_angle'].round(decimals=2)
+
+        return df
 
     def get_penalties(self):
         df = self.game
@@ -284,4 +305,9 @@ class GameClient:
         df['3v4'] = np.where((df['home_players'] == 3)&(df['away_players'] == 4)&(df['attacking_team'] == df['home_team']), 1, 0) +\
                     np.where((df['home_players'] == 4)&(df['away_players'] == 3)&(df['attacking_team'] == df['away_team']), 1, 0)
         
-        return self.game
+        return df
+    
+    def preprocess(self,df):
+        le = LabelEncoder()
+        df["previous_event_type"] = le.fit_transform(df["previous_event_type"])
+        return df
